c4911154d7643d93cb5d4bdec8b74f6312d7024e
VNagorniy <vnagorniyrnd@gmail.com>
2025-06-11 16:32:32 +0300

    feat: add mainLayout component, react-router-dom, global css

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,17 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-import Button from './components/Button/Button';
-import Counter from './Counter';
-import List from './List';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { MainLayout } from './components/MainLayout';
 
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
-		<>
-			<div>
-				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-				<Button onClick={() => setCount((count) => count + 1)}>Click Boy</Button>
-				<Counter />
-				<List />
-			</div>
-		</>
+		<BrowserRouter>
+			<Routes>
+				<Route element={<MainLayout />}>
+					<Route path="/" element={<div>home</div>}></Route>
+					<Route path="/forbidden" element={<div>forbidden page</div>}></Route>
+					<Route path="/addquestion" element={<div>addquestion page</div>}></Route>
+				</Route>
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
---
+++ src/components/MainLayout/MainLayout.jsx
@@ -0,0 +1,21 @@
+import { Outlet } from 'react-router-dom';
+import cls from './MainLayout.module.css';
+
+export const MainLayout = () => {
+	const currentYear = new Date().getFullYear();
+
+	return (
+		<div className={cls.mainLayout}>
+			<header>1</header>
+			<div className={cls.mainWrapper}>
+				<main className={cls.main}>
+					<Outlet />
+				</main>
+				<footer className={cls.footer}>
+					React Question Cards | {currentYear} <br />
+					by NVS_DEV
+				</footer>
+			</div>
+		</div>
+	);
+};
