b5f2cd43c25e1b3b46580be814d54e0096dc6233
VNagorniy <vnagorniyrnd@gmail.com>
2025-06-11 21:15:10 +0300

    feat: add QuestionCard component and page HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { MainLayout } from './components/MainLayout';
+import { HomePage } from './pages/HomePage';
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route element={<MainLayout />}>
-					<Route path="/" element={<div>home</div>}></Route>
+					<Route path="/" element={<HomePage />}></Route>
 					<Route path="/forbidden" element={<div>forbidden page</div>}></Route>
 					<Route path="/addquestion" element={<div>addquestion page</div>}></Route>
 				</Route>
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,11 @@
+import cls from './HomePage.module.css';
+import { QuestionCard } from '../../components/QuestionCard';
+
+export const HomePage = () => {
+	return (
+		<div>
+			HomePage
+			<QuestionCard />
+		</div>
+	);
+};
