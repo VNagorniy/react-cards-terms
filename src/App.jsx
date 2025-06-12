a5714a89140d11efe5ad5d65c22d2b58d4421f05
VNagorniy <vnagorniyrnd@gmail.com>
2025-06-12 21:17:39 +0300

    feat: add QuestionCard props, NotFoundPage component and dynamic routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { MainLayout } from './components/MainLayout';
 import { HomePage } from './pages/HomePage';
+import { NotFoundPage } from './pages/NotFoundPage';
 
 function App() {
 	return (
@@ -10,6 +11,8 @@ function App() {
 					<Route path="/" element={<HomePage />}></Route>
 					<Route path="/forbidden" element={<div>forbidden page</div>}></Route>
 					<Route path="/addquestion" element={<div>addquestion page</div>}></Route>
+					<Route path="/question/:id" element={<div>Question page</div>}></Route>
+					<Route path="*" element={<NotFoundPage />}></Route>
 				</Route>
 			</Routes>
 		</BrowserRouter>
--- src/components/QuestionCard/QuestionCard.jsx
+++ src/components/QuestionCard/QuestionCard.jsx
@@ -1,19 +1,22 @@
+import { useNavigate } from 'react-router-dom';
 import { Button } from '../Button';
 import cls from './QuestionCard.module.css';
 
-export const QuestionCard = () => {
+export const QuestionCard = ({ card }) => {
+	const navigate = useNavigate();
+
 	return (
 		<div className={cls.card}>
 			<div className={cls.cardLabels}>
-				<div>LEvel: 1</div>
-				<div>Not Completed</div>
+				<div>Level: {card.level}</div>
+				<div>{card.completed ? 'Completed' : 'Not Completed'}</div>
 			</div>
-			<h5 className={cls.cardTitle}>Что такое JSX?</h5>
+			<h5 className={cls.cardTitle}>{card.question}</h5>
 			<div className={cls.cardAnswers}>
 				<label>short answer: </label>
-				<p className={cls.cardAnswer}>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</p>
+				<p className={cls.cardAnswer}>{card.answer}</p>
 			</div>
-			<Button onClick={() => {}}>View</Button>
+			<Button onClick={() => navigate(`/question/${card.id}`)}>View</Button>
 		</div>
 	);
 };
