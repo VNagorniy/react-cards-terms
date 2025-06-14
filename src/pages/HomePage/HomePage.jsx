720e9f95dd62bd4831043e4506e39063b2998a71
VNagorniy <vnagorniyrnd@gmail.com>
2025-06-14 21:31:46 +0300

    feat: use memo for QuestionCardList

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,15 @@
 import cls from './HomePage.module.css';
 import { API_URL } from '../../../constants';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { QuestionCardList } from '../../components/QuestionCardList';
 import { Loader } from '../../components/Loader';
 import { useFetch } from '../../hooks/useFetch';
 
 export const HomePage = () => {
 	const [questions, setQuestions] = useState([]);
+	const [searchValue, setSearchValue] = useState('');
+
+	const inputRef = useRef();
 
 	const [getQuestions, isLoading, error] = useFetch(async (url) => {
 		const response = await fetch(`${API_URL}/${url}`);
@@ -20,8 +23,14 @@ export const HomePage = () => {
 		getQuestions('react');
 	}, []);
 
+	const onSearchChangeHandler = (e) => {
+		console.log(e.target.value);
+		setSearchValue(e.target.value);
+	};
+
 	return (
 		<>
+			<input type="text" value={searchValue} onChange={onSearchChangeHandler} />
 			{isLoading && <Loader />}
 			{error && <p>{error}</p>}
 			<QuestionCardList cards={questions} />
