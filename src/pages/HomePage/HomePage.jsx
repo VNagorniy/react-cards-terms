b96c90d8cc5aa6820e1777c41902cea68be76006
VNagorniy <vnagorniyrnd@gmail.com>
2025-06-18 13:12:30 +0300

    feat: add pagination and scroll logics

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -5,11 +5,19 @@ import { QuestionCardList } from '../../components/QuestionCardList';
 import { Loader } from '../../components/Loader';
 import { useFetch } from '../../hooks/useFetch';
 import { SearchInput } from '../../components/SearchInput';
+import { Button } from '../../components/Button';
+
+const DEFAULT_PER_PAGE = 10;
 
 export const HomePage = () => {
-	const [questions, setQuestions] = useState([]);
+	const [searchParams, setSearchParams] = useState(`?_page=1&_per_page=${DEFAULT_PER_PAGE}`);
+	const [questions, setQuestions] = useState({});
 	const [searchValue, setSearchValue] = useState('');
 	const [sortSelectValue, setSortSelectValue] = useState('');
+
+	const controlsContainerRef = useRef();
+
+	const getActivePageNumber = () => (questions.next === null ? questions.last : questions.next - 1);
 
 	const [getQuestions, isLoading, error] = useFetch(async (url) => {
 		const response = await fetch(`${API_URL}/${url}`);
@@ -20,12 +28,27 @@ export const HomePage = () => {
 	});
 
 	const cards = useMemo(() => {
-		return questions.filter((d) => d.question.toLowerCase().includes(searchValue.trim().toLowerCase()));
+		if (questions?.data) {
+			if (searchValue.trim()) {
+				return questions.data.filter((d) => d.question.toLowerCase().includes(searchValue.trim().toLowerCase()));
+			} else {
+				return questions.data;
+			}
+		}
+		return [];
 	}, [questions, searchValue]);
 
+	const pagination = useMemo(() => {
+		const totalCardsCount = questions?.pages || 0;
+
+		return Array(totalCardsCount)
+			.fill(0)
+			.map((_, i) => i + 1);
+	}, [questions]);
+
 	useEffect(() => {
-		getQuestions(`react?${sortSelectValue}`);
-	}, [sortSelectValue]);
+		getQuestions(`react${searchParams}`);
+	}, [searchParams]);
 
 	const onSearchChangeHandler = (e) => {
 		setSearchValue(e.target.value);
@@ -33,11 +56,20 @@ export const HomePage = () => {
 
 	const onSortSelectChangeHandler = (e) => {
 		setSortSelectValue(e.target.value);
+
+		setSearchParams(`?_page=1&_per_page=${DEFAULT_PER_PAGE}&${e.target.value}`);
+	};
+
+	const paginationHandler = (e) => {
+		if (e.target.tagName === 'BUTTON') {
+			setSearchParams(`?_page=${e.target.textContent}&_per_page=${DEFAULT_PER_PAGE}&${sortSelectValue}`);
+			controlsContainerRef.current.scrollIntoView({ behavior: 'smooth' });
+		}
 	};
 
 	return (
 		<>
-			<div className={cls.controlsContainer}>
+			<div className={cls.controlsContainer} ref={controlsContainerRef}>
 				<SearchInput value={searchValue} onChange={onSearchChangeHandler} />
 
 				<select value={sortSelectValue} onChange={onSortSelectChangeHandler} className={cls.select}>
@@ -52,8 +84,21 @@ export const HomePage = () => {
 
 			{isLoading && <Loader />}
 			{error && <p>{error}</p>}
-			{cards.length === 0 && <p className={cls.noCardsInfo}>No cards...</p>}
 			<QuestionCardList cards={cards} />
+
+			{cards.length === 0 ? (
+				<p className={cls.noCardsInfo}>No cards...</p>
+			) : (
+				<div className={cls.paginationContainer} onClick={paginationHandler}>
+					{pagination.map((value) => {
+						return (
+							<Button key={value} isActive={value === getActivePageNumber()}>
+								{value}
+							</Button>
+						);
+					})}
+				</div>
+			)}
 		</>
 	);
 };
