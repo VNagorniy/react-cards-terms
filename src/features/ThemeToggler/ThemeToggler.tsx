cc26670a101ec48311b9a7d42f0888eb7e9e8bb0
VNagorniy <vnagorniyrnd@gmail.com>
2026-06-15 21:05:57 +0300

    refactor: convert ThemeToggler.jsx to ThemeToggler.tsx

--- src/features/ThemeToggler/ThemeToggler.tsx
+++ src/features/ThemeToggler/ThemeToggler.tsx
@@ -1,13 +1,15 @@
+import type { ChangeEvent } from 'react';
 import { THEME_STORAGE } from '../../constants';
 import { useTheme } from '../../hooks/useTheme';
+import { THEME_ENUM } from '../../types/global.types';
 import cls from './ThemeToggler.module.css';
 
 export const ThemeToggler = () => {
 	const { theme, setTheme } = useTheme();
 
-	const onChangeHandler = (e) => {
+	const onChangeHandler = (e: ChangeEvent<HTMLInputElement>): void => {
 		const isChecked = e.target.checked === true;
-		const updatedTheme = e.target.checked === false ? 'light' : 'dark';
+		const updatedTheme = isChecked ? THEME_ENUM.DARK : THEME_ENUM.LIGHT;
 
 		setTheme(updatedTheme);
 		isChecked ? document.body.classList.add('darkLayout') : document.body.classList.remove('darkLayout');
@@ -15,10 +17,10 @@ export const ThemeToggler = () => {
 	};
 
 	return (
-		<label class={cls.switch}>
-			<input type="checkbox" onChange={onChangeHandler} checked={theme === 'dark'} />
-			<span class={cls.slider}></span>
-			<span class={cls.clouds_stars}></span>
+		<label className={cls.switch}>
+			<input type="checkbox" onChange={onChangeHandler} checked={theme === THEME_ENUM.DARK} />
+			<span className={cls.slider}></span>
+			<span className={cls.clouds_stars}></span>
 		</label>
 	);
 };
